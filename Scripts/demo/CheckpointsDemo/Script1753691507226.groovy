fc81020f69c29981f470c023c6a3c4ae9a8514f3
sandhidwiwidyantoro <sandhidwiwidyantoro@gmail.com>
2025-08-21 14:47:52 +0700

    afa

--- Scripts/demo/CheckpointsDemo/Script1753691507226.groovy
+++ Scripts/demo/CheckpointsDemo/Script1753691507226.groovy
@@ -17,5 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+'a'
 WebUI.verifyCheckpoint(findCheckpoint('Checkpoints/CheckpointOne'), true)
 
